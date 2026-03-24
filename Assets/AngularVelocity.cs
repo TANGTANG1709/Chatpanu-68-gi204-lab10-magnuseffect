a7e65a6a5c4751390c488ec1d5c7557e2a225b80
TANGTANG1709 <tvggop@gmail.com>
2026-03-24 12:51:48 +0700

    Angular Velocity&Torque

---
+++ Assets/AngularVelocity.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AngularVelocity : MonoBehaviour
+{
+    public float spinSpeed = 5f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Keyboard.current != null && Keyboard.current.aKey.isPressed)
+        {
+            rb.angularVelocity = new Vector3(0, spinSpeed, 0);
+        }
+        else
+        {
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/TorqueRotate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+// เพิ่มบรรทัดนี้เหมือนเดิมครับ
+using UnityEngine.InputSystem;
+
+public class TorqueRotate : MonoBehaviour
+{
+    public float torqueForce = 10f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Keyboard.current != null && Keyboard.current.dKey.isPressed)
+        {
+            rb.AddTorque(Vector3.forward * torqueForce);
+        }
+    }
+}
